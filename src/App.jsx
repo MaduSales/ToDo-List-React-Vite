d6139ffbfde22d89010a7a5a6d5934bb72712860
Maria Eduarda <maria.eduarda15miranda@gmail.com>
2024-05-10 21:31:37 -0300

    Primeiro Deploy

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ListaDeTarefas from './components/Lista/Lista'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <ListaDeTarefas/>
+    </div>
+    
+
+  )
+}
---
+++ src/components/Lista/Lista.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+
+function ListaDeTarefas(){
+    const [tarefas, setTarefas] = useState([]);
+    const [novaTarefa, setNovaTarefa] = useState("");
+
+    const adicionarTarefa = () =>{
+        if(novaTarefa.trim()!== ""){
+            setTarefas([...tarefas, novaTarefa]);
+            setNovaTarefa("");
+        }
+    };
+
+    const removerTarefa = (index) => {
+        const novasTarefas = [...tarefas];
+        novasTarefas.splice(index, 1); //The index (position) to add or remove items. 
+        setTarefas(novasTarefas);
+    };
+
+    return(
+        <div>
+            <h2>Lista de Tarefas</h2>
+            <input type="text" value={novaTarefa} onChange={(e) => setNovaTarefa(e.target.value)} placeholder="Adicione uma nova tarefa"/>
+            <button onClick={adicionarTarefa}>Adicionar</button>
+
+            <ul>
+                {tarefas.map((tarefa, index) => (
+                    <li key={index}>
+
+                        {tarefa}{" "}
+                        <button className="botão-remover" onClick={() => removerTarefa(index)}>Remover</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+    export default ListaDeTarefas;
